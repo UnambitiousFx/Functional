30f89a064baff787c4e05c5c97c37e1c3dccc201
Oleexo <maxime.charles1@proton.me>
2026-01-10 09:07:04 +0100

    Refactor `ToResult` extension methods to use inner classes, improving encapsulation and consistency. Add `MatchAsync` method for asynchronous `Maybe` handling.

--- src/Functional/MaybeExtensions.ToResult.cs
+++ src/Functional/MaybeExtensions.ToResult.cs
@@ -4,51 +4,47 @@ namespace UnambitiousFx.Functional;
 
 public static partial class MaybeExtensions
 {
-    /// <summary>
-    ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
-    ///     If the Maybe is None, returns Failure with the provided error.
-    /// </summary>
-    /// <typeparam name="TValue">The value type.</typeparam>
     /// <param name="maybe">The maybe instance.</param>
-    /// <param name="error">The error to use when the maybe is None.</param>
-    /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the provided error.</returns>
-    public static Result<TValue> ToResult<TValue>(this Maybe<TValue> maybe, Error error)
-        where TValue : notnull
+    /// <typeparam name="TValue">The value type.</typeparam>
+    extension<TValue>(Maybe<TValue> maybe) where TValue : notnull
     {
-        return maybe.Match(
-            some: Result.Success,
-            none: () => Result.Failure<TValue>(error));
-    }
+        /// <summary>
+        ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
+        ///     If the Maybe is None, returns Failure with the provided error.
+        /// </summary>
+        /// <param name="error">The error to use when the maybe is None.</param>
+        /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the provided error.</returns>
+        public Result<TValue> ToResult(Error error)
+        {
+            return maybe.Match(
+                some: Result.Success,
+                none: () => Result.Failure<TValue>(error));
+        }
 
-    /// <summary>
-    ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
-    ///     If the Maybe is None, returns Failure with an error created by the factory function.
-    /// </summary>
-    /// <typeparam name="TValue">The value type.</typeparam>
-    /// <param name="maybe">The maybe instance.</param>
-    /// <param name="errorFactory">Factory function to create an error when the maybe is None.</param>
-    /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the error from the factory.</returns>
-    public static Result<TValue> ToResult<TValue>(this Maybe<TValue> maybe, Func<Error> errorFactory)
-        where TValue : notnull
-    {
-        return maybe.Match(
-            some: Result.Success,
-            none: () => Result.Failure<TValue>(errorFactory()));
-    }
+        /// <summary>
+        ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
+        ///     If the Maybe is None, returns Failure with an error created by the factory function.
+        /// </summary>
+        /// <param name="errorFactory">Factory function to create an error when the maybe is None.</param>
+        /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the error from the factory.</returns>
+        public Result<TValue> ToResult(Func<Error> errorFactory)
+        {
+            return maybe.Match(
+                some: Result.Success,
+                none: () => Result.Failure<TValue>(errorFactory()));
+        }
 
-    /// <summary>
-    ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
-    ///     If the Maybe is None, returns Failure with an error containing the provided message.
-    /// </summary>
-    /// <typeparam name="TValue">The value type.</typeparam>
-    /// <param name="maybe">The maybe instance.</param>
-    /// <param name="message">The error message to use when the maybe is None.</param>
-    /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the provided message.</returns>
-    public static Result<TValue> ToResult<TValue>(this Maybe<TValue> maybe, string message)
-        where TValue : notnull
-    {
-        return maybe.Match(
-            some: Result.Success,
-            none: () => Result.Failure<TValue>(message));
+        /// <summary>
+        ///     Converts a Maybe to a Result. If the Maybe is Some, returns Success with the value.
+        ///     If the Maybe is None, returns Failure with an error containing the provided message.
+        /// </summary>
+        /// <param name="message">The error message to use when the maybe is None.</param>
+        /// <returns>A Result that is successful if the maybe is Some; otherwise a failure with the provided message.</returns>
+        public Result<TValue> ToResult(string message)
+        {
+            return maybe.Match(
+                some: Result.Success,
+                none: () => Result.Failure<TValue>(message));
+        }
     }
 }
