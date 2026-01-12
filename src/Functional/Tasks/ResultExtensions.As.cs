d6f0943799b549401eab19d592278a7bac4b7af9
Oleexo <maxime.charles1@proton.me>
2026-01-12 20:13:55 +0100

    Add `As` and `AsAsync` extensions for transforming non-generic `Result`. Update tests to cover `Task` and `ValueTask` scenarios. Integrate `AsyncFixer` to improve async code analysis.

--- src/Functional/Tasks/ResultExtensions.As.cs
+++ src/Functional/Tasks/ResultExtensions.As.cs
@@ -30,4 +30,25 @@ public static partial class ResultExtensions
         var result = await awaitableResult;
         return result.As(value);
     }
+
+    /// <summary>
+    /// Transforms a <see cref="Task{TResult}"/> containing a <see cref="Result"/>
+    /// to a new <see cref="Task{TResult}"/> with a specified output value of type <typeparamref name="TOut"/>
+    /// upon success, while retaining the original error information upon failure.
+    /// </summary>
+    /// <typeparam name="TOut">The type of the output value to transform to.</typeparam>
+    /// <param name="awaitableResult">
+    /// The <see cref="Task{TResult}"/> containing a <see cref="Result"/> to be transformed.
+    /// </param>
+    /// <param name="value">The new value of type <typeparamref name="TOut"/> to associate with the resulting success.</param>
+    /// <returns>
+    /// A <see cref="Task{TResult}"/> containing a <see cref="Result{TOut}"/> with the specified output value
+    /// if the original result succeeded, or the original failure information if it failed.
+    /// </returns>
+    public static async Task<Result<TOut>> AsAsync<TOut>(this Task<Result> awaitableResult, TOut value)
+        where TOut : notnull
+    {
+        var result = await awaitableResult;
+        return result.As(value);
+    }
 }
