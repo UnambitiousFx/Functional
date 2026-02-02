be5eb88816c1ac1cfc0e5c8e5467a37721f678d8
Oleexo <maxime.charles1@proton.me>
2026-02-02 19:56:17 +0100

    Standardize parameter casing in `ValidationFailure` constructors.

--- src/Functional/Failures/ValidationFailure.cs
+++ src/Functional/Failures/ValidationFailure.cs
@@ -8,19 +8,24 @@ public sealed record ValidationFailure : Failure
     /// <summary>
     ///     Represents a validation error containing one or more validation failure messages.
     /// </summary>
-    /// <param name="Failures">The collection of validation failure messages.</param>
-    /// <param name="Extra">Optional additional metadata about the validation failures.</param>
-    public ValidationFailure(IReadOnlyList<string> Failures,
-        IReadOnlyDictionary<string, object?>? Extra = null)
-        : base(ErrorCodes.Validation, Failures.Count == 0
+    /// <param name="failures">The collection of validation failure messages.</param>
+    /// <param name="extra">Optional additional metadata about the validation failures.</param>
+    public ValidationFailure(IReadOnlyList<string> failures,
+        IReadOnlyDictionary<string, object?>? extra = null)
+        : base(ErrorCodes.Validation, failures.Count == 0
                 ? "Validation failed."
-                : string.Join("; ", Failures),
-            Merge(Extra, [new KeyValuePair<string, object?>("failures", Failures)]))
+                : string.Join("; ", failures),
+            Merge(extra, [new KeyValuePair<string, object?>("failures", failures)]))
     {
-        this.Failures = Failures;
-        this.Extra = Extra;
+        Failures = failures;
+        Extra = extra;
     }
 
+    /// <summary>
+    ///     Represents a validation error containing one or more validation failure messages.
+    /// </summary>
+    /// <param name="validationMessage">The validation failure message.</param>
+    /// <param name="extra">Optional additional metadata about the validation failures.</param>
     public ValidationFailure(string validationMessage, IReadOnlyDictionary<string, object?>? extra = null)
         : this([validationMessage], extra)
     {
